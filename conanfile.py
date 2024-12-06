7d0242ad30711e747420de21b5f7cb9f1bda9ece
nbqduong <86090755+nbqduong@users.noreply.github.com>
2024-12-06 12:45:15 +0700

    workflow support conan

--- conanfile.py
+++ conanfile.py
@@ -1,13 +1,14 @@
 from conan import ConanFile
 from conan.tools.cmake import cmake_layout
 
-
 class ExampleRecipe(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeDeps", "CMakeToolchain"
 
     def requirements(self):
+        self.requires("fmt/11.0.2")
         self.requires("gtest/1.15.0")
 
     def layout(self):
         cmake_layout(self)
+
